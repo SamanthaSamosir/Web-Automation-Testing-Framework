9019c9cf965a299c36d339f66518b6e41d726748
Samantha <meliorasamantha@gmail.com>
2025-03-17 14:50:35 +0700

    Updated .gitignore and removed unnecessary files

--- data/testData.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    customers: {
-        johnDoe: { firstName: 'John', lastName: 'Doe', postCode: '12345' },
-        janeDoe: { firstName: 'Jane', lastName: 'Doe', postCode: '54321' }
-    }
-};
